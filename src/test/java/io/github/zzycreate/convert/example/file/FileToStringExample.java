d88f11b4ae4bc5531a7ac93c32447a291c99701f
zzycreate <zzycreate@gmail.com>
2019-05-04 00:07:00 +0800

    FileToStringExample 文件转字符串示例

---
+++ src/test/java/io/github/zzycreate/convert/example/file/FileToStringExample.java
@@ -0,0 +1,26 @@
+package io.github.zzycreate.convert.example.file;
+
+import org.junit.Test;
+
+import java.io.File;
+
+/**
+ * 文件对象转字符串
+ * File to String
+ *
+ * @author zzycreate
+ * @date 19-5-3
+ * @see <a href="https://stackoverflow.com/questions/326390/how-do-i-create-a-java-string-from-the-contents-of-a-file">https://stackoverflow.com/questions/326390/how-do-i-create-a-java-string-from-the-contents-of-a-file</a>
+ */
+public class FileToStringExample {
+
+    @Test
+    public void readFileWithBufferReader() {
+
+    }
+
+    private File createFile() {
+        return null;
+    }
+
+}
